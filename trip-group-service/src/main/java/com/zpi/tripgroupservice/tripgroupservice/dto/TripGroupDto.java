ff61602178c884d11015becf82fc9b7983399310
DorianBestrzynski <80275992+DorianBestrzynski@users.noreply.github.com>
2022-09-14 18:03:19 +0200

    Added input length validation from dto

--- trip-group-service/src/main/java/com/zpi/tripgroupservice/tripgroupservice/dto/TripGroupDto.java
+++ trip-group-service/src/main/java/com/zpi/tripgroupservice/tripgroupservice/dto/TripGroupDto.java
@@ -1,21 +1,26 @@
 package com.zpi.tripgroupservice.tripgroupservice.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.zpi.tripgroupservice.tripgroupservice.commons.Currency;
+import org.hibernate.validator.constraints.Length;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 public record TripGroupDto(
         @NotEmpty
+        @Length(max = 20)
         @JsonProperty("name")
         String name,
         @NotNull
         @JsonProperty("currency")
         Currency currency,
+        @Length(max = 120)
         @JsonProperty("description")
         String description,
         @JsonProperty("votesLimit")
         Integer votesLimit,
         @NotEmpty
+        @Length(max = 100)
         @JsonProperty("startLocation")
         String startLocation) {
 }
