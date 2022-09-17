907233dfd14bbbdb436ccd3abc023a9e9a5c6431
DorianBestrzynski <80275992+DorianBestrzynski@users.noreply.github.com>
2022-09-17 19:10:53 +0200

    Ref

--- user-service/src/main/java/com/zpi/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/zpi/userservice/UserServiceApplication.java
@@ -1,19 +1,11 @@
 package com.zpi.userservice;
-
-import com.zpi.userservice.user.AppUser;
-import com.zpi.userservice.user.AppUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
