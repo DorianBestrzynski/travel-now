5f0b6dc763f4093433adcd71af358f508681de73
DorianBestrzynski <80275992+DorianBestrzynski@users.noreply.github.com>
2022-10-16 19:40:46 +0200

    Added post and get for user accommodation propsal

--- transport-service/src/main/java/com/zpi/transportservice/transportservice/transport/TransportService.java
+++ transport-service/src/main/java/com/zpi/transportservice/transportservice/transport/TransportService.java
@@ -4,14 +4,19 @@ import com.zpi.transportservice.transportservice.accommodation_transport.Accommo
 import com.zpi.transportservice.transportservice.adapter.LufthansaAdapter;
 import com.zpi.transportservice.transportservice.dto.AccommodationInfoDto;
 import com.zpi.transportservice.transportservice.dto.TripDataDto;
+import com.zpi.transportservice.transportservice.dto.UserTransportDto;
 import com.zpi.transportservice.transportservice.flight.Flight;
 import com.zpi.transportservice.transportservice.flight.FlightService;
+import com.zpi.transportservice.transportservice.mapper.MapStructMapper;
 import com.zpi.transportservice.transportservice.proxy.AccommodationProxy;
 import com.zpi.transportservice.transportservice.proxy.TripGroupProxy;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.time.*;
 import java.util.*;
@@ -22,12 +27,12 @@ import java.util.*;
 @Slf4j
 public class TransportService {
     private final TransportRepository transportRepository;
-    private final AirTransportRepository airTransportRepository;
     private final AccommodationTransportService accommodationTransportService;
     private final TripGroupProxy tripGroupProxy;
     private final AccommodationProxy accommodationProxy;
     private final LufthansaAdapter lufthansaAdapter;
     private final FlightService flightService;
+    private final MapStructMapper mapper;
 
     @Transactional
     public List<Transport> getTransportForAccommodation(Long accommodationId){
@@ -38,12 +43,18 @@ public class TransportService {
                 .toList();
         var accommodationInfo = accommodationProxy.getAccommodationInfo(accommodationId);
         var tripInfo = tripGroupProxy.getTripData(accommodationInfo.groupId());
+        var userTransport = getUserTransport(accommodationTransportIdList);
         var airTransport = generateTransportForAccommodationAir(accommodationTransportIdList, accommodationInfo, tripInfo, accommodationId);
         //TODO here car transport logic and return type List<Transport> with best AirTransport and CarTransport
         if(airTransport != null){
             transports.add(airTransport);
         }
+        transports.addAll(userTransport);
         return transports;
+    }
+
+    private List<UserTransport> getUserTransport(List<Long> transportList) {
+        return transportRepository.findUserTransport(transportList);
     }
 
     public AirTransport generateTransportForAccommodationAir(List<Long> accommodationTransportIds, AccommodationInfoDto accommodationInfo, TripDataDto tripData, Long accommodationId) {
@@ -70,7 +81,7 @@ public class TransportService {
     }
 
     private AirTransport shouldGenerateTransportAir(List<Long> accommodationTransportIds, AccommodationInfoDto accommodationInfo, TripDataDto tripData, Long accommodationId) {
-        var transportAir = airTransportRepository.findAllById(accommodationTransportIds);
+        var transportAir = transportRepository.findTransportAir(accommodationTransportIds);
         if(transportAir.isEmpty()) {
             var matchingAccommodationTransportAir = transportRepository.findMatchingTransportAir(tripData.startingLocation(),
                     accommodationInfo.city(), tripData.startDate());
@@ -83,6 +94,13 @@ public class TransportService {
         return transportAir.get(0);
     }
 
-
-
+    @Transactional
+    public UserTransport createUserTransport(Long accommodationId, @Valid UserTransportDto userTransportDto) {
+        UserTransport userTransport = new UserTransport(userTransportDto.duration(), userTransportDto.price(), userTransportDto.source(),
+                userTransportDto.destination(), userTransportDto.startDate(), userTransportDto.endDate(), userTransportDto.link(),
+                userTransportDto.meanOfTransport(), userTransportDto.description(), userTransportDto.meetingTime());
+        var userTransportSaved = transportRepository.save(userTransport);
+        accommodationTransportService.createAccommodationTransport(accommodationId, userTransportSaved.getTransportId());
+        return userTransportSaved;
+    }
 }
