f682bd4f9764b84f18b09a0cbbf9bde22e07ecb2
Kajetan <kajtusb@gmail.com>
2022-10-16 12:12:28 +0200

    Extracting data from places api

--- day-plan-service/src/main/java/com/zpi/dayplanservice/attraction/AttractionService.java
+++ day-plan-service/src/main/java/com/zpi/dayplanservice/attraction/AttractionService.java
@@ -26,22 +26,25 @@ public class AttractionService {
         return null;
     }
 
-    public Attraction addAttraction(AttractionDto attractionDto) {
+    public List<AttractionCandidateDto> findCandidates(AttractionDto attractionDto) {
+        List<AttractionCandidateDto> result = new ArrayList<>();
         try {
             var foundCandidates = PlacesApi.findPlaceFromText(context, attractionDto.name(), FindPlaceFromTextRequest.InputType.TEXT_QUERY)
                                            .fields(
                                                    FindPlaceFromTextRequest.FieldMask.NAME,
                                                    FindPlaceFromTextRequest.FieldMask.GEOMETRY,
                                                    FindPlaceFromTextRequest.FieldMask.PHOTOS,
-                                                   FindPlaceFromTextRequest.FieldMask.OPENING_HOURS)
+                                                   FindPlaceFromTextRequest.FieldMask.OPENING_HOURS,
+                                                   FindPlaceFromTextRequest.FieldMask.PLACE_ID)
                                            .await();
 
-//            var placeUrl = PlacesApi.placeDetails(context)
+            result = convertToAttractionCandidateDto(foundCandidates.candidates);
+            getUrlAndOpeningHours(result);
             System.out.println(foundCandidates);
         } catch (ApiException | InterruptedException | IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return result;
     }
 
     public Attraction deleteAccommodation(Long attractionId, Long dayPlanId) {
@@ -68,16 +71,19 @@ public class AttractionService {
     private List<AttractionCandidateDto> getUrlAndOpeningHours(List<AttractionCandidateDto> candidates)  {
         for (AttractionCandidateDto candidate : candidates) {
             try {
-                var placeDetails = PlacesApi.placeDetails(context, candidate.placeId())
+                var placeDetails = PlacesApi.placeDetails(context, candidate.getPlaceId())
                                             .fields(PlaceDetailsRequest.FieldMask.OPENING_HOURS,
-                                                    PlaceDetailsRequest.FieldMask.WEBSITE)
+                                                    PlaceDetailsRequest.FieldMask.URL,
+                                                    PlaceDetailsRequest.FieldMask.FORMATTED_ADDRESS)
                                             .await();
-                candidate.url() = placeDetails.url;
+                candidate.setUrl(placeDetails.url.toString());
+                candidate.setOpeningHours(placeDetails.openingHours.weekdayText);
+                candidate.setAddress(placeDetails.formattedAddress);
             } catch (ApiException | InterruptedException | IOException e) {
                 throw new RuntimeException(e);
             }
         }
-        return result;
+        return candidates;
     }
 
 }
