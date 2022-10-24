9b5de60e8b6bb3e6655d52118c62432b5790da85
dorek <dorian.bestrzynski@gmail.com>
2022-10-24 11:56:04 +0200

    Security refactor

---
+++ api-gateway/src/main/java/security/SecurityConfig.java
@@ -0,0 +1,75 @@
+package security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenFilter jwtTokenFilter;
+    private final UserDetailsService userDetailsService;
+    private final Environment environment;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable()
+            .csrf().disable();
+
+        configureAuthentication(http);
+    }
+
+    private void configureAuthentication(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint((request, response, authException) -> {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+            });
+
+        if (Arrays.asList(environment.getActiveProfiles()).contains("developer")) {
+            enableSwaggerAccess(http);
+        }
+
+        http.authorizeRequests()
+            .antMatchers("/api/auth/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    private void enableSwaggerAccess(HttpSecurity http) throws Exception {
+        http.headers()
+            .frameOptions().disable()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/swagger-ui/**", "/swagger-ui.html").permitAll();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
---
+++ api-gateway/src/main/java/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package security;
+
+import com.zpi.userservice.user.AppUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService
+{
+    private final AppUserService appUserService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var appUser = appUserService.getAppUserByEmail(username);
+
+        return new User(appUser.getEmail(), appUser.getPassword().getHashedPassword(), Collections.emptyList());
+    }
+}
