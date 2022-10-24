2010a74382fba62e8821d9f99b6667c8e868e6e0
BoBa <baranskikajtek@gmail.com>
2022-10-24 23:30:32 +0200

    xddd

--- transport-service/src/test/java/com/zpi/transportservice/TransportServiceApplicationTests.java
+++ transport-service/src/test/java/com/zpi/transportservice/TransportServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zpi.transportservice.transportservice;
+package com.zpi.transportservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
