626fafef144221152ec02f07a10e8841bb070b45
Kajetan <kajtusb@gmail.com>
2022-11-12 11:34:45 +0100

    Authorization for availability and attractions

--- availability-service/src/main/java/com/zpi/availabilityservice/proxies/TripGroupProxy.java
+++ availability-service/src/main/java/com/zpi/availabilityservice/proxies/TripGroupProxy.java
@@ -3,6 +3,7 @@ package com.zpi.availabilityservice.proxies;
 import com.zpi.availabilityservice.dto.AvailabilityConstraintsDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "trip-group", url = "${app.group-service}:8082/api/v1/trip-group")
@@ -10,4 +11,10 @@ public interface TripGroupProxy {
 
     @GetMapping("/availability-info")
     AvailabilityConstraintsDto getAvailabilityConstraints(@RequestParam Long groupId);
+
+    @GetMapping("/user-group/role")
+    boolean isUserCoordinator(@RequestHeader("innerCommunication") String header, @RequestParam(name = "groupId")Long groupId, @RequestParam(name = "userId")Long userId);
+
+    @GetMapping( "/user-group/group")
+    Boolean isUserPartOfTheGroup(@RequestHeader("innerCommunication") String header, @RequestParam("groupId") Long groupId, @RequestParam("userId") Long userId);
 }
