a9a231b5d482bfac787acb6f2a00d41e6ddd10ef
BoBa <baranskikajtek@gmail.com>
2022-11-19 22:13:36 +0100

    List of group participants and coordinators

---
+++ trip-group-service/src/main/java/com/zpi/tripgroupservice/dto/UserDto.java
@@ -0,0 +1,4 @@
+package com.zpi.tripgroupservice.dto;
+
+public record UserDto(Long userId, String username, String firstName, String lastName) {
+}
--- trip-group-service/src/main/java/com/zpi/tripgroupservice/user_group/UserGroupRepository.java
+++ trip-group-service/src/main/java/com/zpi/tripgroupservice/user_group/UserGroupRepository.java
@@ -1,5 +1,6 @@
 package com.zpi.tripgroupservice.user_group;
 
+import com.zpi.tripgroupservice.commons.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ import java.util.List;
 public interface UserGroupRepository extends JpaRepository<UserGroup, UserGroupKey> {
 
     List<UserGroup> findAllById_GroupId(Long groupId);
+    List<UserGroup> findAllById_GroupIdAndRoleEquals(Long id_groupId, Role role);
 
     Integer countAllById_GroupId(Long groupId);
 }
