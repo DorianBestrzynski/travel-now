bae5ace738ef2a08d150636495231257e00be1e2
DorianBestrzynski <80275992+DorianBestrzynski@users.noreply.github.com>
2022-11-20 22:34:21 +0100

    Added setted shared availability

--- trip-group-service/src/main/java/com/zpi/tripgroupservice/dto/TripExtendedDataDto.java
+++ trip-group-service/src/main/java/com/zpi/tripgroupservice/dto/TripExtendedDataDto.java
@@ -3,6 +3,7 @@ package com.zpi.tripgroupservice.dto;
 import com.zpi.tripgroupservice.commons.Currency;
 import com.zpi.tripgroupservice.commons.GroupStage;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
@@ -10,5 +11,5 @@ import java.time.LocalDate;
 public record TripExtendedDataDto(String name, Currency currency, String description, Integer votesLimit,
                                   String startLocation, String startCity, LocalDate startDate, LocalDate endDate,
                                   Double latitude, Double longitude, GroupStage groupStage, Integer minimalNumberOfDays,
-                                  Integer minimalNumberOfParticipants, Long selectedAccommodationId, Integer participantsNum) {
+                                  Integer minimalNumberOfParticipants, Long selectedAccommodationId, Long selectedSharedAvailability, Integer participantsNum) {
 }
