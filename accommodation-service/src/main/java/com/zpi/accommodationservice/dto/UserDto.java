0eda51f7c3beeb151fb09bf54073c8d2ecc94788
BoBa <baranskikajtek@gmail.com>
2022-11-20 20:47:44 +0100

    Added integrations test for accommodation user

--- accommodation-service/src/main/java/com/zpi/accommodationservice/dto/UserDto.java
+++ accommodation-service/src/main/java/com/zpi/accommodationservice/dto/UserDto.java
@@ -1,4 +1,8 @@
 package com.zpi.accommodationservice.dto;
 
 public record UserDto(Long userId, String phoneNumber, String firstName, String lastName) {
+    public UserDto()
+    {
+        this(null, null, null, null);
+    }
 }
