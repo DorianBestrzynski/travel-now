f464a48106463539381effc12f5ce3a8579478e5
piter <piterm33@gmail.com>
2022-11-29 20:26:26 +0100

    Unified user dtos

--- availability-service/src/main/java/com/zpi/availabilityservice/dto/UserDto.java
+++ availability-service/src/main/java/com/zpi/availabilityservice/dto/UserDto.java
@@ -1,6 +1,6 @@
 package com.zpi.availabilityservice.dto;
 
-public record UserDto(Long userId, String phoneNumber, String email, String firstName, String lastName) {
+public record UserDto(Long userId, String phoneNumber, String email, String firstName, String surname) {
     public UserDto()
     {
         this(null, null, null, null, null);
