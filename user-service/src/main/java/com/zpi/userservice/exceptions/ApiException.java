99d5ad0c5a33d5fec95b88728d0e561e37468de3
BoBa <baranskikajtek@gmail.com>
2022-12-04 13:59:18 +0100

    Exceptions fix

---
+++ user-service/src/main/java/com/zpi/userservice/exceptions/ApiException.java
@@ -0,0 +1,15 @@
+package com.zpi.userservice.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+public record ApiException(
+        @JsonProperty("message")
+        String message,
+        @JsonProperty("status")
+        HttpStatus status,
+        @JsonProperty("timestamp")
+        ZonedDateTime timestamp) {
+}
--- user-service/src/main/java/com/zpi/userservice/exceptions/ApiExceptionsHandler.java
+++ user-service/src/main/java/com/zpi/userservice/exceptions/ApiExceptionsHandler.java
@@ -7,28 +7,36 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
 import java.net.ConnectException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ApiExceptionsHandler {
+    public final static ZoneId ZONE_ID = ZoneId.of("Europe/Warsaw");
 
     @ExceptionHandler(value = {IllegalArgumentException.class})
     public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return handleExceptions(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {EntityNotFoundException.class})
     public ResponseEntity<Object> handleResourceNotFoundException(EntityNotFoundException e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        return handleExceptions(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = {ConnectException.class})
     public ResponseEntity<Object> handleConnectException(ConnectException e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+        return handleExceptions(e.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
     }
 
     @ExceptionHandler(value = {NoSuchElementException.class})
     public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        return handleExceptions(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    private ResponseEntity<Object> handleExceptions(String message, HttpStatus request){
+        ApiException apiException = new ApiException(message, request, ZonedDateTime.now(ZONE_ID));
+        return new ResponseEntity<>(apiException, request);
     }
 }
