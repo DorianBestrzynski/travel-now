7dc7214210decaddb591f2de79ba5531e6fbd27e
DorianBestrzynski <80275992+DorianBestrzynski@users.noreply.github.com>
2022-12-06 02:36:55 +0100

    Revert "Dates test"
    This reverts commit 099adbcda8cea0ab6df00febee57594220cd4a57.

--- trip-group-service/src/main/java/com/zpi/tripgroupservice/TripGroupServiceApplication.java
+++ trip-group-service/src/main/java/com/zpi/tripgroupservice/TripGroupServiceApplication.java
@@ -1,16 +1,11 @@
 package com.zpi.tripgroupservice;
-
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.google.maps.GeoApiContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -25,18 +20,6 @@ public class TripGroupServiceApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
-	}
-
-	private static final String dateFormat = "yyyy-MM-dd";
-	private static final String dateTimeFormat = "yyyy-MM-dd HH:mm:ss";
-
-	@Bean
-	public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
-		return builder -> {
-			builder.simpleDateFormat(dateTimeFormat);
-			builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
-			builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
-		};
 	}
 
 }
