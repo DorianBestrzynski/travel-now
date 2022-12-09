32fb990afd5a248d821b1ec1576ca4431aaebac5
BoBa <baranskikajtek@gmail.com>
2022-12-09 16:24:58 +0100

    Day plan bug fixed

--- day-plan-service/src/main/java/com/zpi/dayplanservice/day_plan/DayPlanService.java
+++ day-plan-service/src/main/java/com/zpi/dayplanservice/day_plan/DayPlanService.java
@@ -65,7 +65,7 @@ public class DayPlanService {
             throw new IllegalArgumentException(INVALID_DAY_PLAN_ID);
         }
 
-        if(!isDateAvailable(dayPlanDto))
+        if(dayPlanRepository.findDayPlanByDate(dayPlanDto.date()).getDayPlanId().equals(dayPlanId))
             throw new IllegalDateException(TAKEN_DATE);
 
         var dayPlan = dayPlanRepository.findById(dayPlanId)
